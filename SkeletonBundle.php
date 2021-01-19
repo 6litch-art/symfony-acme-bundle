ec3eed3ee7d0dcfd5cb14e32bbb984d93e462212
Marco Meyer (www) <marco.meyerconde@gmail.com>
2021-01-19 11:40:25 +0100

    Update SkeletonBundle

---
+++ SkeletonBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace xKzl\SkeletonBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SkeletonBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
